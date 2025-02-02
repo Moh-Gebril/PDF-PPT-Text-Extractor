f42b078fa54f54a3be1df30c1d45a00c7f5778b4
Mohamed Gebril <mohamed.gebril@crc.com>
2025-02-02 15:05:58 +0200

    Update pptx output format

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/gebril/MyWorkSpace/Codes/Git-Hub/pdf_ppt_text_extractor/venv
+set -gx VIRTUAL_ENV /home/gebril/MyWorkSpace/Codes/Git-Hub/PDF-PPT-Text-Extractor/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
